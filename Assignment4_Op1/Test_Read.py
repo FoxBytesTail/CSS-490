1ab12bce692b42a80b2cd3723d1b77114bb3ad7c
Aelfrix <aelfriclee@gmail.com>
2017-06-06 16:01:04 -0700

    bug fixes to zero cross point calc. Some additions

--- Assignment4_Op1/Test_Read.py
+++ Assignment4_Op1/Test_Read.py
@@ -8,16 +8,39 @@ import matplotlib.pyplot as plt
 import numpy as n
 
 #Returns an array of features
-def extract_feature(data):
-    pass
-
+def extract_features(data):
+    energy = get_energy(data)
+    cross_rate = get_zeroCrossRate(data)
+    bandwidth = get_bandwidth(data)
+    spectral_centroid = get_spectral_centroid(data)
+    
+    feature_array = n.zeros((1,4), dtype='f')
+    
+    for row in xrange(n.shape(feature_array)[0]):
+        feature_array[row][0] = energy
+        feature_array[row][1] = cross_rate
+        feature_array[row][2] = bandwidth
+        feature_array[row][3] = spectral_centroid
+        
+    print
+    print "Feature Array"
+    print feature_array
+            
+    
+    
 def get_energy(data):
-    return n.cumsum(data**2)[-1]
+    return n.cumsum(data**2)[-1]/len(data)
     
 def get_zeroCrossRate(data):
-    sum = 0
+    sum = 0.0
     n = len(data)
     
+    #zero cross rate
+    #(1 - 1) + ... for positive, positive
+    #(1 + 1) + ... for positive, negative
+    #(-1 -1) + ... for negative, positive
+    #(-1 + 1) + ... for negative, negative
+
     for i in xrange(n-1):
         global sign_a
         global sign_b
@@ -28,14 +51,24 @@ def get_zeroCrossRate(data):
             sign_a = -1
         if data[i+1] < 0:
             sign_b = 1
-            
-        sum = sum + sign_a + sign_b
         
-    return sum/(2*(n-1))
+        sum = sum + abs(sign_a + sign_b)
+        
+    return sum/(2.0*(n-1))
     
 def get_bandwidth(data):
-    return len(data)/2 - 1
+    fft_data = fft(data)
     
+    firstPeak = max(fft_data[0:len(fft_data)/2])
+    secondPeak = max(fft_data[len(fft_data)/2:])
+    
+    l_data = list(fft_data)
+    min_freq = l_data.index(firstPeak) 
+    l_data.remove(firstPeak)
+    max_freq = l_data.index(secondPeak) + 1
+    
+    return max_freq - min_freq
+        
 def get_spectral_centroid(data):
     full_spectrum = abs(fft(data)[:(len(data)/2-1)])
     
@@ -47,28 +80,29 @@ def get_spectral_centroid(data):
     
 if __name__ == '__main__':
     fs, data = wavfile.read('./audio/music/mu1.wav')
+    #fs, data = wavfile.read('./audio/speech/sp3.wav')
     
     print "Sampling Frequency " + str(fs)
     print "Amplitudes " + str(data)
     print "Number of samples " + str(len(data))
-    print data[0:16000:1000]
     
-    plt.subplot(2,1,1)
+    plt.subplot(3,1,1)
     plt.title('Input File')
     plt.ylabel("Amplitude")
     plt.xlabel("Num Samples")
     plt.plot(data, 'g')
 
-
-    get_spectral_centroid(data)
+    extract_features(data)
     
     c = fft(data) # calculate fourier transform (complex numbers list)
     d = len(c)/2  # you only need half of the fft list (real signal symmetry)
 
-    plt.subplot(2,1,2)
+    plt.subplot(3,1,2)
     plt.title('Spectrum')
     plt.xlabel("Frequency")
     plt.ylabel("dB?")
-    plt.plot(abs(c[:(d-1)]),'r') 
+    plt.plot(c[:(len(c)-1)],'r')     
+    
     plt.show()
+    
     
